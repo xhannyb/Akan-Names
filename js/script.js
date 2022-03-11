4d49fbdfe7f126dfbeaab2034c9e96cfa21cd32d
Shannon Bulimo <shannonbulimo@gmail.com>
2022-03-11 08:36:07 +0300

    alert message

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+alert("welcome");
 var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
